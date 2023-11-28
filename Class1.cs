8468699cbc77f59e0add7b0db265c4b6e62f90ee
mohammedaljaafari2019 <mohmmed1423@hotmail.com>
2023-11-28 22:12:35 +0300

    first class

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public abstract class Directory
+{
+	private string Name;
+    private int Size;
+	public string getName() {  return Name; }
+	public int getSize() { return size; }
+	public void setName(string name) { Name = name;}
+	public void setSize(int size) { size = Size; }
+
+	
+}
